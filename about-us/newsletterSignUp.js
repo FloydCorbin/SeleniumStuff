1d76ac10b720a70730f47a11248f23d25e6b8341
flody-der <floyd.corbin@gmail.com>
2020-12-08 09:52:35 -0700

    template for test, tile animation selectors

---
+++ about-us/newsletterSignUp.js
@@ -0,0 +1,109 @@
+const drupalBasePages = [
+    'https://innovation.org',
+    'https://innovation.org/about-us',
+    'https://innovation.org/combatting-childhood-cancer',
+    'https://innovation.org/diseases',
+    'https://innovation.org/latest-articles',
+    'https://innovation.org/sign-up',
+    'https://innovation.org/study-shows-lung-cancer-survival-rates-improving-thanks-new-treatments',
+    'https://innovation.org/tackling-worlds-number-one-killer',
+    'https://innovation.org/how-cancer-research-helping-develop-covid-19-treatments',
+    'https://innovation.org/takeaction',
+    'https://innovation.org/terms-of-use',
+    'https://innovation.org/votersforcures',
+    'https://innovation.org/votersforcures-h-r-3-is-the-wrong-approach-for-patients-like-me',
+    'https://innovation.org/when-supporting-medical-innovation-becomes-personal-for-patients',
+]
+
+const sitecoreBasePages = [
+    'https://staging-innovation.mrmdigital.com',
+    'https://staging-innovation.mrmdigital.com/about-us',
+    'https://staging-innovation.mrmdigital.com/combatting-childhood-cancer',
+    'https://staging-innovation.mrmdigital.com/diseases',
+    'https://staging-innovation.mrmdigital.com/how-cancer-research-helping-develop-covid-19-treatments',
+    'https://staging-innovation.mrmdigital.com/latest-articles',
+    'https://staging-innovation.mrmdigital.com/sign-up',
+    'https://staging-innovation.mrmdigital.com/study-shows-lung-cancer-survival-rates-improving-thanks-new-treatments',
+    'https://staging-innovation.mrmdigital.com/tackling-worlds-number-one-killer',
+    'https://staging-innovation.mrmdigital.com/takeaction',
+    'https://staging-innovation.mrmdigital.com/terms-of-use',
+    'https://staging-innovation.mrmdigital.com/votersforcures',
+    'https://staging-innovation.mrmdigital.com/votersforcures-h-r-3-is-the-wrong-approach-for-patients-like-me',
+    'https://staging-innovation.mrmdigital.com/when-supporting-medical-innovation-becomes-personal-for-patients',
+]
+
+const {
+    Builder,
+    Capabilities,
+    By,
+    Key,
+    until
+} = require('selenium-webdriver');
+const driver = new Builder().forBrowser('firefox').build();
+const caps = new Capabilities();
+caps.setPageLoadStrategy('eager');
+
+// SELECTORS (CSS)
+
+const selectorDr = {
+    
+}
+
+const selectorSc = {
+
+}
+
+
+//TARGET ARRAYS
+let arrayDr = [];
+let arraySc = [];
+
+
+/*DRUPAL*/
+async function mainDr() {
+
+    try {
+
+        await driver.get(drupalBasePages[1]);
+        
+
+        console.log(arrayDr);
+
+        if (arraySc.toString === arrayDr.toString)  {
+            console.log('The arrays match in length')
+            for (let i = 0; i < arraySc.length; i++) {
+                console.log(arrayDr[i]);
+                console.log(arraySc[i]);  
+            };
+        } else {
+            console.log('The text doesn\'t match');
+        }
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+        await (await driver).quit();
+
+    }
+}
+
+/*SITECORE*/
+async function mainSc() {
+
+    try {
+        await driver.get(sitecoreBasePages[1]);
+
+        
+
+
+
+        console.log(arraySc)
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+        mainDr();
+    }
+}
+
+mainSc();
